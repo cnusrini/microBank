88932f8bde79c79b8257c1f924a32ee6ec7db0cd
myopenleder <cnu.srini@hotmail.com>
2018-12-18 10:08:33 -0500

    added negative tests

--- test/enrolledTests.js
+++ test/enrolledTests.js
@@ -3,14 +3,26 @@ var assert = require('assert');
 
 contract('Unit test for isEnrolled?', function(accounts) {
 
+  const owner = accounts[0];
   const cust1 = accounts[1];
+  let deployedContract;
+
+  beforeEach('ITs deploy SimpleBank smart contract', async () => {
+    deployedContract = await SimpleBank.deployed();
+  });
 
   it('tests for positive test', async () => {
-    const deployedContract = await SimpleBank.deployed();
+
     const enrolled = await deployedContract.enroll({from: cust1});
     const didCustEnrolled = await deployedContract.enrolled(cust1);
 
     assert.equal(didCustEnrolled, true, "ssss");
 
+  });
+
+  it('tests for negative test', async () => {
+    const isOnwerEnrolled = await deployedContract.enrolled(owner);
+
+    assert.equal(isOnwerEnrolled, false);
   });
 });
